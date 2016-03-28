92c86eb6c7354d96bd735f6a0950eaebfabcb339
xufang <datouren2008@yeah.net>
2016-03-28 17:16:44 +0800

    这次直接是git commit -m “然后写这段注释”,但是发现如果这样做的话，hello.c不会改变，因为我没有吧modified:hello.c前面的注释号去掉，所以我又加了 一句git add hello.c试试

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@ int main(void)
 {
   printf("hello20150328\n");
   printf("I am learning how to update from git\n");
+  printf("准备加上readme.txt来记录，这次使用git commit -m来直接写注释然后git push，看效果如何");
   return 0;
 }
