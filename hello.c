051bc642fd428308dec0c79177db9a26796ccd2e
xufang <datouren2008@yeah.net>
2016-03-28 17:24:24 +0800

    这次先git add hello.c 然后git commit -m 最后git push

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@ int main(void)
 {
   printf("hello20150328\n");
   printf("I am learning how to update from git\n");
-  printf("准备加上readme.txt来记录，这次使用git commit -m来直接写注释然后git push，看效果如何");
+  printf("上次直接修改.c后使用git commit -m "注释"提交到本地仓库，然后git push的话没能成功，因为没有在提交前把modified:hello.c这句的注释去掉");
+　printf("这次的做法是：修改.c后直接git add　hello.c　然后git commit -m "xxx",看下最后能不能push成功");
   return 0;
 }
